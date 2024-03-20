3a6ee9f1a69883fd553179e5ff90416aeb350888
Khalid-debugg <khalidsherif.dev@gmail.com>
2024-03-20 21:55:54 +0200

    chore: Update Appwrite configuration :memo:

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,13 +1,18 @@
 import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
-  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  projectID: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   url: import.meta.env.VITE_APPWRITE_URL,
+  storageID: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  databaseID: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  mygamesID: import.meta.env.VITE_APPWRITE_MYGAMES_ID,
+  usersID: import.meta.env.VITE_APPWRITE_USERS_ID,
+  postsID: import.meta.env.VITE_APPWRITE_POSTS_ID,
 };
 export const client = new Client();
 
 client.setEndpoint(appwriteConfig.url);
-client.setProject(appwriteConfig.projectId);
+client.setProject(appwriteConfig.projectID);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
