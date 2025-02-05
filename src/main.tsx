e61dd1d2af14c5ee65732043a8d648ee0894849a
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-02-05 16:49:15 +0200

    fix(Main) preventing refetching on tab change

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,14 @@ import AuthProvider from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
