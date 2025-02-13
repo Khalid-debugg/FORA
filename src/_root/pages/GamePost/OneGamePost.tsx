c1231df242fe33e8f017eedd1ca5bdc08b950968
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-02-13 17:29:03 +0200

    fix(SideNav & AuthContex) Display user's name

--- src/_root/pages/GamePost/OneGamePost.tsx
+++ src/_root/pages/GamePost/OneGamePost.tsx
@@ -33,6 +33,7 @@ const OneGamePost = () => {
   const { mutateAsync: deleteGame } = useDeleteGamePost();
   const navigate = useNavigate();
   const [isEditing, setIsEditing] = useState(false);
+
   const handleDelete = async () => {
     const response = await deleteGame(id);
     if (!response) {
@@ -51,51 +52,8 @@ const OneGamePost = () => {
   };
 
   return (
-    <div className="flex flex-col gap-2 md:w-1/3 w-full mx-auto items-center">
-      <div className="flex items-center justify-between p-4 w-full gap-4 shadow-md">
-        <div className="flex items-center">
-          <button
-            onClick={() => navigate(-1)}
-            className="hover:bg-gray-100 p-4 rounded-md"
-          >
-            <FaArrowLeft size={25} color="green" />
-          </button>
-          <h1 className="text-xl">Game</h1>
-        </div>
-        <AlertDialog>
-          {user.id === post?.creator.$id && (
-            <DropdownMenu label={<SlOptions size={25} color="green" />}>
-              <DropdownMenuItem onClick={() => setIsEditing(true)}>
-                Edit
-              </DropdownMenuItem>
-              <AlertDialogTrigger>
-                <DropdownMenuItem onClick={undefined}>Delete</DropdownMenuItem>
-              </AlertDialogTrigger>
-            </DropdownMenu>
-          )}
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                game from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onClick={handleDelete}>
-                Delete
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </div>
-      {isEditing ? (
-        <EditGamePost post={post} setIsEditing={setIsEditing} />
-      ) : (
-        <GamePost post={post} isOne />
-      )}
-      {isPending && (
+    <>
+      {isPending ? (
         <div className="flex flex-col space-y-3">
           <Skeleton className="h-[125px] w-[250px] rounded-xl" />
           <div className="space-y-2">
@@ -103,8 +61,54 @@ const OneGamePost = () => {
             <Skeleton className="h-4 w-[200px]" />
           </div>
         </div>
-      )}{" "}
-    </div>
+      ) : (
+        <div className="flex flex-col gap-2 md:w-1/3 w-full mx-auto items-center">
+          <div className="flex items-center justify-between p-4 w-full gap-4 shadow-md">
+            <div className="flex items-center">
+              <button
+                onClick={() => navigate(-1)}
+                className="hover:bg-gray-100 p-4 rounded-md"
+              >
+                <FaArrowLeft size={25} color="green" />
+              </button>
+              <h1 className="text-xl">Game</h1>
+            </div>
+            <AlertDialog>
+              {user.id === post?.creator.$id && (
+                <DropdownMenu label={<SlOptions size={25} color="green" />}>
+                  <DropdownMenuItem onClick={() => setIsEditing(true)}>
+                    Edit
+                  </DropdownMenuItem>
+                  <AlertDialogTrigger>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </AlertDialogTrigger>
+                </DropdownMenu>
+              )}
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your game from our servers.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction onClick={handleDelete}>
+                    Delete
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+          {isEditing ? (
+            <EditGamePost post={post} setIsEditing={setIsEditing} />
+          ) : (
+            <GamePost post={post} isOne />
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
